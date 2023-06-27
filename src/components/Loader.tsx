4fb5bc58e2af0c546885cc1501872c5b9d443cbd
carloeusebi <carloeusebi@gmail.com>
2023-06-27 15:16:01 +0200

    Completed layout

---
+++ src/components/Loader.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <figure id="loader">
+      <img src="pokeball.png" alt="Loading.." />
+    </figure>
+  );
+};
+
+export default Loader;
---
+++ src/components/Screen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Loader from "./Loader";
+
+const Screen = () => {
+  return (
+    <section id="screen">
+      <Loader />
+    </section>
+  );
+};
+
+export default Screen;
